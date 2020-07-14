6891ce581dcaa50dd2b8cc1595705b519c8ad070
biplab37 <biplabmahato37@gmail.com>
2020-07-14 22:19:53 +0530

    separated the parts into modules

--- coupled.jl
+++ coupled.jl
@@ -3,19 +3,15 @@
 ###    points. Here we have introduced a cutoff for the Bososnic momenta as well.     ###
 #########################################################################################
 
-## package for numerical integration
-using QuadGK
-
 ## Initialisation
-const m = 198 # number of cutoffs
-const n = 209 # number of momenta
 
-const dcutoff = 1.0/m
+const m::Int64 = 198 # number of cutoffs
+const n::Int64 = 209 # number of momenta
 
 velocity = zeros(n,m)
 dielectric = zeros(n,m)
 
-"""
+@doc raw"""
     velocity_integrand(momentum, cutoff, phi, m, n, i)
 
 This function returns the integrand of the FRG equation for the velocity renormlisation.
@@ -27,7 +23,7 @@ This function returns the integrand of the FRG equation for the velocity renorml
     n          (Int64) : number of momenta
     i          (Int64) : index for the running cutoff
 """
-function velocity_integrand(momentum,cutoff,phi,m,n,i)
+function velocity_integrand(dielectric::Array{Float64,2}, momentum::Float64, cutoff::Float64, phi::Float64, m::Int64, n::Int64, i::Int64)
     ## Theta function implementation with conditional
     if cos(phi)<=1 - 2*cutoff/momentum
         return 0.0
@@ -35,22 +31,22 @@ function velocity_integrand(momentum,cutoff,phi,m,n,i)
         k1 = cutoff
         k2 = cutoff + cos(phi)*momentum
 
-        index1 = Int(round(k1*n))
-        index2 = Int(round(k2*n))
+        index1::Int64 = Int(round(k1*n))
+        index2::Int64 = Int(round(k2*n))
 
-        eps1 = dielectric[index1,m-i+2]
+        eps1::Float64 = dielectric[index1,m-i+2]
 
         if index2<n
-            eps2 = dielectric[index2,m-i+2]
+            eps2::Float64 = dielectric[index2,m-i+2]
         else
-            eps2 = 1.0 # if the index goes out of the boundary take dielectric to be free space one.
+            eps2::Float64 = 1.0 # if the index goes out of the boundary take dielectric to be free space one.
         end
 
         return 2.2*((momentum^2 - k1^2 + k2^2)/(momentum^2*eps1) + (momentum^2 + k1^2 - k2^2)/(momentum^2*eps2))/(2.0*pi*sqrt((k1+k2)^2 - momentum^2))
     end
 end
 
-"""
+@doc raw"""
     dielectric_integrand(momentum, cutoff, phi, m, n, i)
 
 This function returns the integrand of the FRG equation for the dielectric function renormlisation.
@@ -62,52 +58,39 @@ This function returns the integrand of the FRG equation for the dielectric funct
     n          (Int64) : number of momenta
     i          (Int64) : index for the running cutoff
 """
-function dielectric_integrand(momentum,cutoff,phi,m,n,i)
+function dielectric_integrand(velocity::Array{Float64,2}, momentum::Float64, cutoff::Float64, phi::Float64, m::Int64, n::Int64, i::Int64)
     ## Theta function implementation
     if cos(phi)<=1 - 2*cutoff/momentum
         return 0.0
     else
         k1 = cutoff
         k2 = cutoff + cos(phi)*momentum
-        index1 = Int(round(k1*n))
-        index2 = Int(round(k2*n))
 
-        vel1 = velocity[index1,m-i+2]
+        index1::Int64 = Int64(round(k1*n))
+        index2::Int64 = Int64(round(k2*n))
+
+        vel1::Float64 = velocity[index1,m-i+2]
 
         if index2<n
-            vel2 = velocity[index2,m-i+2]
+            vel2::Float64 = velocity[index2,m-i+2]
         else
-            vel2 = 1.0
+            vel2::Float64 = 1.0
         end
 
         return 4.4*momentum*sin(phi)^2/(pi*(k1*vel1 + k2*vel2)*sqrt((k1+k2)^2 - momentum^2))
     end
 end
 
-## boundary values initialisation
+## Boundary values initialisation
 velocity[:,m] .= 1.0
 dielectric[:,m] .= 1.0
 
-## The main for loops that implements the RG procedure
-for i in 2:m
+## solving exact FRG using an user defined function
+using .RGProcedure
 
-    cutoff = Float64(m - i +1)/m
-
-    for j in 1:n
-
-        momentum = Float64(j)/n
-
-        velocity_integrand_phi(phi) = velocity_integrand(momentum,cutoff,phi,m,n,i)
-        dielectric_integrand_phi(phi) = dielectric_integrand(momentum,cutoff,phi,m,n,i)
-
-        ## Solving ODE's using Euler method
-        velocity[j,m-i+1] = velocity[j,m-i+2] + dcutoff*quadgk(velocity_integrand_phi,0.,pi/2.,rtol=1e-3)[1]
-        dielectric[j,m-i+1] = dielectric[j,m-i+2] + dcutoff*quadgk(dielectric_integrand_phi,0.,pi/2.,rtol=1e-3)[1]
-    end
-end
+rg_procedure(velocity,dielectric,velocity_integrand, dielectric_integrand ,m,n)
 
 ## Plots using a user defined module
-
 using .Plotting
 
 plot_velocity(velocity[:,1],"Renormalised_velocity.pdf")
